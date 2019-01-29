ad1818a5c578b2ecd8a09e71da634c5d60d313a2
Ronda Wylie <rondawylie@yahoo.com>
2019-01-29 14:33:54 -0600

    fade in and out

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
-// bubbly({
-//     colorStart: "#fff",
-//     colorStop: "#fff",
-//     blur: 1,
-//     compose: "source-over",
-//     bubbleFunc: () => `hsla(${Math.random() * 50}, 100%, 50%, .2)`
-// });
+bubbly({
+    colorStart: "#fff",
+    colorStop: "#fff",
+    blur: 1,
+    compose: "source-over",
+    bubbleFunc: () => `hsla(${Math.random() * 50}, 100%, 50%, .2)`
+});
+
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+
+        var height = $('.first-container').height();
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop >= height - 40) {
+            $('.nav-container').addClass('solid-nav', 'logo');
+        } else {
+            $('.nav-container').removeClass('solid-nav');
+        }
+
+    });
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 0) {
+        $('#logo').fadeOut();
+      }
+      else {
+        $('#logo').fadeIn();
+      }
+    });
+});
+
+$(function() {
+    $('body').removeClass('fade-out');
+});
